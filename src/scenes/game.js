4dcf2902c9e1f1ea44497f8554b8f136e34e2988
Rian M <62781175+xyzrmatti@users.noreply.github.com>
2022-01-17 08:42:12 -0500

    Update game.js

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -10,13 +10,19 @@ export default class Game extends Phaser.Scene {
 
     preload ()
     {
-        this.load.image('sky', 'src/assets/ground.png');
+        this.load.image('sky', 'src/assets/sky.png');
         this.load.image('ground', 'src/assets/ground.png');
+        // this.load.spritesheet('dude', 'src/games/firstgame/assets/dude.png', { frameWidth: 32, frameHeight: 48 }); player spritesheet
+        this.load.image('player1', 'src/assets/player1.png');
+        this.load.image('cloud1', 'src/assets/cloud1.png');
+        this.load.image('cloud2', 'src/assets/cloud2.png');
+        this.load.image('cloud3', 'src/assets/cloud3.png');
     }
       
     create ()
     {
-        this.add.image(400, 300, 'sky');
+        this.add.image(0, 0, 'sky').setOrigin(0, 0);
+        this.add.image()
         
         // ground = this.physics.add.staticGroup();
         // ground.create(60, 300, 'ground').scaleRatio();
