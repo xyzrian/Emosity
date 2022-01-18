62f0b177da24108bcc53690aa5640a5b5b0926bb
Rian M <62781175+xyzrmatti@users.noreply.github.com>
2022-01-18 15:56:54 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import Phaser from 'phaser'
 import Boot from './scenes/boot'
 import Game from './scenes/game'
 
-var scaleRatio = window.devicePixelRatio / 3; //load in assets, scale with myAsset.scale.setTo(scaleRatio, scaleRatio);
+// var scaleRatio = window.devicePixelRatio / 3; //load in assets, scale with myAsset.scale.setTo(scaleRatio, scaleRatio);
 
 const config = {
     title: "Emosity",
     version: "0.0.1",
+    parent: "phaser-game",
     type: Phaser.AUTO,
-    // width: window.innerWidth * window.devicePixelRatio,
-    // height: window.innerHeight * window.devicePixelRatio,
-    width: 480,
-    height: 270,
+    width: window.innerWidth * window.devicePixelRatio,
+    height: window.innerHeight * window.devicePixelRatio,
+    // width: 480,
+    // height: 270,
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+    },
     physics: {
         default: 'arcade',
         arcade: {
