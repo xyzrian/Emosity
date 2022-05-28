1921ff50917881133ae49b41143d69eaa19b7789
Rian M <62781175+xyzrian@users.noreply.github.com>
2022-05-27 23:28:54 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import Phaser from 'phaser'
+import Boot from './scenes/boot'
+import Intro from './scenes/intro'
+import Game from './scenes/game'
+import Outro from './scenes/outro'
+
+const config = {
+    title: "Emosity",
+    version: "0.0.1",
+    type: Phaser.AUTO,
+    pixelArt: true,
+    width: 480,
+    height: 270,
+    scale: {
+        mode: Phaser.Scale.ENVELOP,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        parent: "phaser-game"
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y:450 },
+            debug: false
+        }
+    },
+    scene: [Boot, Intro, Game, Outro]
+};
+
+var game = new Phaser.Game(config);
+// window.game = game;
